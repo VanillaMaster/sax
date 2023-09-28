406e9970e665ac30e8bb5537af4500e0a6988dd8
VM <vanillaflamaster@gmail.com>
2023-09-28 22:42:04 +0300

    implement script parsing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import sample from "./sample.js";
-
 /** ```>``` */
 const CHAR_GREATER = 62;//">".charCodeAt(0);
 /** ```<``` */
@@ -14,6 +12,8 @@ const CHAR_QUOTATION_DOUBLE = 34;//'"'.charCodeAt(0);
 const CHAR_SLASH = 47;//"/".charCodeAt(0);
 /** ```-``` */
 const CHAR_MINUS = 45;//"-".charCodeAt(0);
+/** ```:``` */
+const CHAR_COLON = ":".charCodeAt(0);
 /** ```A``` */
 const CHAR_A_UPPER = 65;//"A".charCodeAt(0);
 /** ```Z``` */
@@ -30,6 +30,18 @@ const CHAR_9 = 57;//"9".charCodeAt(0);
 const CHAR_SPACE = 32;//" ".charCodeAt(0);
 /** ```\n``` */
 const CHAR_NEW_LINE = 10;//"\n".charCodeAt(0);
+/** ```s``` */
+const CHAR_S_LOWER = "s".charCodeAt(0);
+/** ```c``` */
+const CHAR_C_LOWER = "c".charCodeAt(0);
+/** ```r``` */
+const CHAR_R_LOWER = "r".charCodeAt(0);
+/** ```i``` */
+const CHAR_I_LOWER = "i".charCodeAt(0);
+/** ```p``` */
+const CHAR_P_LOWER = "p".charCodeAt(0);
+/** ```t``` */
+const CHAR_T_LOWER = "t".charCodeAt(0);
 
 function isLetter(char: number) {
     return (CHAR_A_UPPER <= char && char <= CHAR_Z_UPPER) || (CHAR_A_LOWER <= char && char <= CHAR_Z_LOWER);
@@ -40,7 +52,7 @@ function isDigit(char: number) {
 }
 
 function isAlphanumeric(char: number) {
-    return isLetter(char) || isDigit(char) || char == CHAR_MINUS;
+    return isLetter(char) || isDigit(char) || char == CHAR_MINUS || char == CHAR_COLON;
 }
 
 function isSpace(char: number) {
@@ -61,8 +73,35 @@ enum STATE {
     COMMENT_NODE_END_1 = "COMMENT_NODE_END_1",
     COMMENT_NODE_END_2 = "COMMENT_NODE_END_2",
 
+    NODE_START_SCRIPT_1 = "NODE_START_SCRIPT_1",
+    NODE_START_SCRIPT_2 = "NODE_START_SCRIPT_2",
+    NODE_START_SCRIPT_3 = "NODE_START_SCRIPT_3",
+    NODE_START_SCRIPT_4 = "NODE_START_SCRIPT_4",
+    NODE_START_SCRIPT_5 = "NODE_START_SCRIPT_5",
+    NODE_START_SCRIPT_6 = "NODE_START_SCRIPT_6",
+    
+    NODE_SCRIPT_BODY = "NODE_SCRIPT_BODY",
+
+    NODE_SCRIPT_ATTR_NAME = "NODE_SCRIPT_ATTR_NAME",
+    NODE_SCRIPT_ATTR_VALUE_START = "NODE_SCRIPT_ATTR_VALUE_START",
+    NODE_SCRIPT_ATTR_VALUE_BODY = "NODE_SCRIPT_ATTR_VALUE_BODY",
+
+    SCRIPT_BODY = "SCRIPT_BODY",
+    
+    NODE_END_SCRIPT_1 = "NODE_END_SCRIPT_1",
+    NODE_END_SCRIPT_2 = "NODE_END_SCRIPT_2",
+    NODE_END_SCRIPT_3 = "NODE_END_SCRIPT_3",
+    NODE_END_SCRIPT_4 = "NODE_END_SCRIPT_4",
+    NODE_END_SCRIPT_5 = "NODE_END_SCRIPT_5",
+    NODE_END_SCRIPT_6 = "NODE_END_SCRIPT_6",
+    NODE_END_SCRIPT_7 = "NODE_END_SCRIPT_7",
+    NODE_END_SCRIPT_8 = "NODE_END_SCRIPT_8",
+
+
+
     NODE_NAME = "NODE_NAME",
     NODE_BODY = "NODE_BODY",
+    NODE_BODY_SELF_CLOSING = "NODE_BODY_SELF_CLOSING",
 
     NODE_ATTR_NAME = "NODE_ATTR_NAME",
 
@@ -103,6 +142,10 @@ class Automata {
                 this.state = STATE.CLOSING_NODE
             } else if (symbol == CHAR_EXCLAMATION_MARK) {
                 this.state = STATE.META_NODE;
+            } else if (symbol == CHAR_S_LOWER) {
+                this.state = STATE.NODE_START_SCRIPT_1;
+
+                this.buffer.push(symbol);
             } else if (isLetter(symbol)) {
                 this.state = STATE.NODE_NAME;
 
@@ -110,6 +153,238 @@ class Automata {
                 console.log("OPENING_NODE");
             } else throw new Error(`unexpected symbol (${String.fromCharCode(symbol)})`);
         },
+        
+        /**
+         * ```<s```
+         */
+        [STATE.NODE_START_SCRIPT_1](symbol: number) {
+            if (symbol == CHAR_C_LOWER) {
+                this.state = STATE.NODE_START_SCRIPT_2;
+
+                this.buffer.push(symbol);
+            } else Automata.stateBinding[STATE.NODE_NAME].call(this, symbol);
+        },
+
+        /**
+         * ```<sc```
+         */
+        [STATE.NODE_START_SCRIPT_2](symbol: number) {
+            if (symbol == CHAR_R_LOWER) {
+                this.state = STATE.NODE_START_SCRIPT_3;
+
+                this.buffer.push(symbol);
+            } else Automata.stateBinding[STATE.NODE_NAME].call(this, symbol);
+        },
+
+        /**
+         * ```<scr```
+         */
+        [STATE.NODE_START_SCRIPT_3](symbol: number) {
+            if (symbol == CHAR_I_LOWER) {
+                this.state = STATE.NODE_START_SCRIPT_4;
+
+                this.buffer.push(symbol);
+            } else Automata.stateBinding[STATE.NODE_NAME].call(this, symbol);
+        },
+
+        /**
+         * ```<scri```
+         */
+        [STATE.NODE_START_SCRIPT_4](symbol: number) {
+            if (symbol == CHAR_P_LOWER) {
+                this.state = STATE.NODE_START_SCRIPT_5;
+
+                this.buffer.push(symbol);
+            } else Automata.stateBinding[STATE.NODE_NAME].call(this, symbol);
+        },
+
+        /**
+         * ```<scrip```
+         */
+        [STATE.NODE_START_SCRIPT_5](symbol: number) {
+            if (symbol == CHAR_T_LOWER) {
+                this.state = STATE.NODE_START_SCRIPT_6;
+
+                this.buffer.push(symbol);
+            } else Automata.stateBinding[STATE.NODE_NAME].call(this, symbol);
+        },
+
+        /**
+         * ```<script```
+         */
+        [STATE.NODE_START_SCRIPT_6](symbol: number) {
+            if (isSpace(symbol)) {
+                this.state = STATE.NODE_SCRIPT_BODY;
+                
+                console.log("SCRIPT");
+                this.buffer.length = 0;
+            } else if (symbol == CHAR_GREATER) {
+                this.state = STATE.SCRIPT_BODY;
+
+                console.log("SCRIPT");
+                this.buffer.length = 0;
+            } else Automata.stateBinding[STATE.NODE_NAME].call(this, symbol);
+        },
+
+        [STATE.NODE_SCRIPT_BODY](symbol: number) {
+            if (symbol == CHAR_GREATER) {
+                this.state = STATE.SCRIPT_BODY;
+            } else if (isLetter(symbol)) {
+                this.state = STATE.NODE_SCRIPT_ATTR_NAME;
+
+                this.buffer.push(symbol);
+            } else if (isSpace(symbol)) {
+                //ok
+            } else throw new SyntaxError(`unexpected symbol (${String.fromCharCode(symbol)})`);
+        },
+
+        [STATE.NODE_SCRIPT_ATTR_NAME](symbol: number){
+            if (isAlphanumeric(symbol)) {
+                //ok
+                this.buffer.push(symbol);
+            } else if (symbol == CHAR_EQUAL) {
+                this.state = STATE.NODE_SCRIPT_ATTR_VALUE_START;
+
+                console.log("NODE_ATTR_NAME:", String.fromCharCode(...this.buffer));
+                this.buffer.length = 0;
+            } else if (isSpace(symbol)) {
+                this.state = STATE.NODE_SCRIPT_BODY;
+
+                console.log("NODE_ATTR_NAME:", String.fromCharCode(...this.buffer));
+                this.buffer.length = 0;
+            } else if (symbol == CHAR_GREATER) {
+                this.state = STATE.SCRIPT_BODY;
+
+                console.log("NODE_ATTR_NAME:", String.fromCharCode(...this.buffer));
+                this.buffer.length = 0;
+            } else throw new SyntaxError(`unexpected symbol (${String.fromCharCode(symbol)})`);
+        },
+
+        [STATE.NODE_SCRIPT_ATTR_VALUE_START](symbol: number) {
+            if (symbol == CHAR_QUOTATION_DOUBLE) {
+                this.state = STATE.NODE_SCRIPT_ATTR_VALUE_BODY;
+            } else throw new SyntaxError(`unexpected symbol (${String.fromCharCode(symbol)})`);
+        },
+
+        [STATE.NODE_SCRIPT_ATTR_VALUE_BODY](symbol: number) {
+            if (symbol == CHAR_QUOTATION_DOUBLE) {
+                this.state = STATE.NODE_SCRIPT_BODY;
+
+                console.log("NODE_ATTR_VALUE:", String.fromCharCode(...this.buffer));
+                this.buffer.length = 0;
+            } else {
+                this.buffer.push(symbol);
+            }
+        },
+        
+        [STATE.SCRIPT_BODY](symbol: number) {
+            if (symbol == CHAR_LESS) {
+                this.state = STATE.NODE_END_SCRIPT_1;
+
+                this.buffer.push(symbol);
+            } else {
+                //ok
+                this.buffer.push(symbol);
+            }
+        },
+
+        [STATE.NODE_END_SCRIPT_1](symbol: number) {
+            if (symbol == CHAR_SLASH) {
+                this.state = STATE.NODE_END_SCRIPT_2;
+
+                this.buffer.push(symbol);
+            } else {
+                this.state = STATE.SCRIPT_BODY;
+
+                this.buffer.push(symbol);
+            }
+        },
+
+        [STATE.NODE_END_SCRIPT_2](symbol: number) {
+            if (symbol == CHAR_S_LOWER) {
+                this.state = STATE.NODE_END_SCRIPT_3;
+
+                this.buffer.push(symbol);
+            } else {
+                this.state = STATE.SCRIPT_BODY;
+
+                this.buffer.push(symbol);
+            }
+        },
+
+        [STATE.NODE_END_SCRIPT_3](symbol: number) {
+            if (symbol == CHAR_C_LOWER) {
+                this.state = STATE.NODE_END_SCRIPT_4;
+
+                this.buffer.push(symbol);
+            } else {
+                this.state = STATE.SCRIPT_BODY;
+
+                this.buffer.push(symbol);
+            }
+        },
+
+        [STATE.NODE_END_SCRIPT_4](symbol: number) {
+            if (symbol == CHAR_R_LOWER) {
+                this.state = STATE.NODE_END_SCRIPT_5;
+
+                this.buffer.push(symbol);
+            } else {
+                this.state = STATE.SCRIPT_BODY;
+
+                this.buffer.push(symbol);
+            }
+        },
+
+        [STATE.NODE_END_SCRIPT_5](symbol: number) {
+            if (symbol == CHAR_I_LOWER) {
+                this.state = STATE.NODE_END_SCRIPT_6;
+
+                this.buffer.push(symbol);
+            } else {
+                this.state = STATE.SCRIPT_BODY;
+
+                this.buffer.push(symbol);
+            }
+        },
+
+        [STATE.NODE_END_SCRIPT_6](symbol: number) {
+            if (symbol == CHAR_P_LOWER) {
+                this.state = STATE.NODE_END_SCRIPT_7;
+
+                this.buffer.push(symbol);
+            } else {
+                this.state = STATE.SCRIPT_BODY;
+
+                this.buffer.push(symbol);
+            }
+        },
+
+        [STATE.NODE_END_SCRIPT_7](symbol: number) {
+            if (symbol == CHAR_T_LOWER) {
+                this.state = STATE.NODE_END_SCRIPT_8;
+
+                this.buffer.push(symbol);
+            } else {
+                this.state = STATE.SCRIPT_BODY;
+
+                this.buffer.push(symbol);
+            }
+        },
+
+        [STATE.NODE_END_SCRIPT_8](symbol: number) {
+            if (symbol == CHAR_GREATER) {
+                this.state = STATE.NONE;
+
+                this.buffer.length -= 8;
+                console.log("SCRIPT:", String.fromCharCode(...this.buffer));
+                this.buffer.length = 0;
+            } else {
+                this.state = STATE.SCRIPT_BODY;
+
+                this.buffer.push(symbol);
+            }
+        },        
 
         /**
          * ```<!```
@@ -235,6 +510,17 @@ class Automata {
                 this.buffer.push(symbol);
             } else if (isSpace(symbol)) {
                 //ok
+            } else if (symbol == CHAR_SLASH) {
+                this.state = STATE.NODE_BODY_SELF_CLOSING;
+                
+            } else throw new SyntaxError(`unexpected symbol (${String.fromCharCode(symbol)})`);
+        },
+
+        [STATE.NODE_BODY_SELF_CLOSING](symbol: number) {
+            if (symbol == CHAR_GREATER) {
+                this.state = STATE.NONE;
+
+                console.log("NODE_SELF_CLOSING");
             } else throw new SyntaxError(`unexpected symbol (${String.fromCharCode(symbol)})`);
         },
 
@@ -318,6 +604,10 @@ class Automata {
         }
     }
 }
+
+import { readFile } from "node:fs/promises";
+const sample = await readFile("C:/Downloads/1.html", "utf-8");
+// console.log(sample);
 
 const a = new Automata();
 a.pushChunk(sample);
--- src/sample.ts
+++ src/sample.ts
@@ -7,6 +7,26 @@ export default `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" data-attr2="42">
     <title>Document</title>
+    <script data-a data="yes">
+        function ajax(id,snum) {
+            var num=snum -2;
+            if (num<0) {num=0}
+            $('.kactive').removeClass('kactive');
+            $('#scroll').scrollTo("#" +num , 500, {axis:'x'});
+            $("#" +snum).addClass('kactive');
+            $.cookie(window.location.pathname,snum, { expires: 365 });
+            jQuery.ajax({        
+                crossDomain: true,
+                url:     "/frame.php?play=" + id, 
+                type:     "GET", 
+                dataType: "html", 
+                success: function(response){ 
+                    $('#player').html(response);
+                }
+            });
+       }
+    
+    </script>
 </head>
 <body>
     <!-- asd -->
