6a4d307d2c6eb99a4e21fbe7eff7815cbb6f8af3
Vanilla <vanillaflamaster@gmail.com>
2023-10-02 16:21:54 +0300

    update example code

--- example/index.js
+++ example/index.js
@@ -1,19 +1,6 @@
 
 import { Builder, Automata, DocumentParser } from "../dist/index.js";
 
-/**
- * @param { Response } resp 
- * @returns 
- */
-async function* read(resp) {
-    const reader = resp.body.getReader();
-    while(true) {
-        const {done, value} = await reader.read();
-        if (done) return;
-        yield value;
-    }
-}
-
 // console.time("parse");
 const parser = new DocumentParser();
 const automata = new Automata(new Builder(parser));
@@ -31,8 +18,17 @@ parser.on("div", (elem) => {
 
 const decoder = new TextDecoder("utf8")
 const resp = await fetch("https://v2.vost.pw/");
+const iter = {
+    reader: resp.body.getReader(),
+    next(){
+        return this.reader.read()
+    },
+    [Symbol.asyncIterator]() {
+        return this
+    }
+}
 
-for await (const chunk of read(resp)) {
+for await (const chunk of iter) {
     automata.pushChunk(decoder.decode(chunk, { stream: true }));
 }
 automata.pushChunk(decoder.decode()).finalize();
